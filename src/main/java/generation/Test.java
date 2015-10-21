1c4b0f5e4aed64355ae150d9cd8d4e7031ca4a6b
Tyler Yates <tyler.yates@utexas.edu>
2015-10-21 12:31:02 -0500

    Use noun and prepositional phrase

--- src/main/java/generation/Test.java
+++ src/main/java/generation/Test.java
@@ -5,6 +5,8 @@ import simplenlg.features.Tense;
 import simplenlg.framework.NLGElement;
 import simplenlg.framework.NLGFactory;
 import simplenlg.lexicon.Lexicon;
+import simplenlg.phrasespec.NPPhraseSpec;
+import simplenlg.phrasespec.PPPhraseSpec;
 import simplenlg.phrasespec.SPhraseSpec;
 import simplenlg.realiser.english.Realiser;
 
@@ -14,9 +16,19 @@ public class Test {
         NLGFactory nlgFactory = new NLGFactory(lexicon);
         Realiser realiser = new Realiser(lexicon);
 
+        final PPPhraseSpec prepositionPhrase = nlgFactory.createPrepositionPhrase();
+        prepositionPhrase.setObject("the United States");
+        prepositionPhrase.setPreposition("of");
+
+        final NPPhraseSpec npPhraseSpec = nlgFactory.createNounPhrase();
+        npPhraseSpec.setNoun("president");
+        npPhraseSpec.setDeterminer("the");
+        npPhraseSpec.addModifier("first");
+        npPhraseSpec.addModifier(prepositionPhrase);
+
         final SPhraseSpec clause = nlgFactory.createClause();
         clause.setSubject("who");
-        clause.setObject("the first president of the United States");
+        clause.setObject(npPhraseSpec);
         clause.setVerb("is");
         clause.setFeature(Feature.TENSE, Tense.PAST);
 
