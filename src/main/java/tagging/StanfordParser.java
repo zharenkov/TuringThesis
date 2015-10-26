7bacc4920d26cd101f426c9bbb93819e1d0793d2
Tyler Yates <tyler.yates@utexas.edu>
2015-10-26 15:08:59 -0500

    Create test class for Stanford parser

---
+++ src/main/java/tagging/StanfordParser.java
@@ -0,0 +1,30 @@
+package tagging;
+
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.parser.lexparser.LexicalizedParser;
+import edu.stanford.nlp.process.CoreLabelTokenFactory;
+import edu.stanford.nlp.process.PTBTokenizer;
+import edu.stanford.nlp.process.Tokenizer;
+import edu.stanford.nlp.process.TokenizerFactory;
+import edu.stanford.nlp.trees.Tree;
+
+import java.io.StringReader;
+import java.util.List;
+
+public class StanfordParser {
+    private final static String PCG_MODEL = "edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz";
+
+    private final TokenizerFactory<CoreLabel> tokenizerFactory = PTBTokenizer.factory(new CoreLabelTokenFactory(), "invertible=true");
+
+    private final LexicalizedParser parser = LexicalizedParser.loadModel(PCG_MODEL);
+
+    public Tree parse(String str) {
+        final List<CoreLabel> tokens = tokenize(str);
+        return parser.apply(tokens);
+    }
+
+    private List<CoreLabel> tokenize(String str) {
+        Tokenizer<CoreLabel> tokenizer = tokenizerFactory.getTokenizer(new StringReader(str));
+        return tokenizer.tokenize();
+    }
+}
---
+++ src/main/java/tagging/Test.java
@@ -0,0 +1,14 @@
+package tagging;
+
+import com.google.common.base.Joiner;
+import edu.stanford.nlp.trees.Tree;
+
+public class Test {
+    final static StanfordParser parser = new StanfordParser();
+
+    public static void main(String[] args) {
+        final String sentence = Joiner.on(' ').join(args);
+        final Tree tree = parser.parse(sentence);
+        System.out.println(tree);
+    }
+}
