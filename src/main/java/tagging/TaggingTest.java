582543548621d8f15ec4e69dc9808fd84a214f95
Tyler Yates <tyler.yates@utexas.edu>
2015-10-30 15:28:37 -0500

    Break up sentence into its parts

--- src/main/java/tagging/TaggingTest.java
+++ src/main/java/tagging/TaggingTest.java
@@ -3,12 +3,31 @@ package tagging;
 import com.google.common.base.Joiner;
 import edu.stanford.nlp.trees.Tree;
 
+import java.util.List;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
 public class TaggingTest {
     final static StanfordParser parser = new StanfordParser();
 
     public static void main(String[] args) {
+        checkArgument(args.length > 0, "Sentence must have at least one word");
+
+        // Wait for the English model to load in order to keep debug text out of the results
+        try {
+            Thread.sleep(500L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         final String sentence = Joiner.on(' ').join(args);
-        final Tree tree = parser.parse(sentence);
-        System.out.println(tree);
+        System.out.println("Sentence being analyzed: " + sentence + "\n---------------------------");
+
+        final Tree tree = parser.parse(sentence).getChild(0);
+        final List<Tree> trees = tree.getChildrenAsList();
+        for (final Tree part : trees) {
+            System.out.print(part.label());
+            System.out.println(part);
+        }
     }
 }
