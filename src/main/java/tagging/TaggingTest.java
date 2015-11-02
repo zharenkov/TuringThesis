355527d132b1acbcbbdceefeb68915bad6ee3b9b
Tyler Yates <tyler.yates@utexas.edu>
2015-11-02 16:35:12 -0600

    Read in the topic sentences if no sentence is given to the program

--- src/main/java/tagging/TaggingTest.java
+++ src/main/java/tagging/TaggingTest.java
@@ -3,25 +3,43 @@ package tagging;
 import com.google.common.base.Joiner;
 import edu.stanford.nlp.trees.Tree;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URL;
 import java.util.List;
+import java.util.Scanner;
 
 import static com.google.common.base.Preconditions.checkArgument;
 
 public class TaggingTest {
     final static StanfordParser parser = new StanfordParser();
 
-    public static void main(String[] args) {
-        checkArgument(args.length > 0, "Sentence must have at least one word");
+    public static void main(String[] args) throws FileNotFoundException {
+        if(args.length == 0) {
+            final URL topicSentences = TaggingTest.class.getResource("/topic_sentences.txt");
+            final Scanner scanner = new Scanner(new File(topicSentences.getFile()));
+            while(scanner.hasNext()) {
+                final String sentence = scanner.nextLine();
+                printSentenceParseTree(sentence);
+            }
+        } else {
 
-        // Wait for the English model to load in order to keep debug text out of the results
-        try {
-            Thread.sleep(500L);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            // Wait for the English model to load in order to keep debug text out of the results
+            try {
+                Thread.sleep(500L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            final String sentence = Joiner.on(' ').join(args);
+            printSentenceParseTree(sentence);
         }
+    }
 
-        final String sentence = Joiner.on(' ').join(args);
+    private static void printSentenceParseTree(String sentence) {
         System.out.println("Sentence being analyzed: \"" + sentence + "\"\n---------------------------");
+
+        sentence = sentence.replaceAll("\\.", "");
 
         final Tree tree = parser.parse(sentence);
         final List<Tree> trees = tree.getChild(0).getChildrenAsList();
@@ -36,5 +54,6 @@ public class TaggingTest {
         for(final Tree leaf : leaves) {
             System.out.printf("(%s - %s), ", leaf.parent(tree).label(), leaf);
         }
+        System.out.println("\n");
     }
 }
