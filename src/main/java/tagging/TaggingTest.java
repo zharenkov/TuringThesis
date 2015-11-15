866d80e93e4262d8cea88a5ab570eff35b0191cb
Tyler Yates <tyler.yates@utexas.edu>
2015-11-15 15:59:16 -0600

    Bring back the step view of the parse tree

--- src/main/java/tagging/TaggingTest.java
+++ src/main/java/tagging/TaggingTest.java
@@ -4,25 +4,41 @@ import com.google.common.base.Joiner;
 import edu.stanford.nlp.trees.Tree;
 import lemmatizing.Lemmatizer;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URL;
 import java.util.List;
-
-import static com.google.common.base.Preconditions.checkArgument;
+import java.util.Scanner;
 
 public class TaggingTest {
     final static StanfordParser parser = new StanfordParser();
 
-    public static void main(String[] args) {
-        checkArgument(args.length > 0, "Sentence must have at least one word");
+    public static void main(String[] args) throws FileNotFoundException {
+        if (args.length == 0) {
+            final URL topicSentences = TaggingTest.class.getResource("/topic_sentences.txt");
+            final Scanner scanner = new Scanner(new File(topicSentences.getFile()));
+            while (scanner.hasNext()) {
+                final String sentence = scanner.nextLine();
+                printSentenceParseTree(sentence);
+            }
+        } else {
 
-        // Wait for the English model to load in order to keep debug text out of the results
-        try {
-            Thread.sleep(500L);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            // Wait for the English model to load in order to keep debug text out of the results
+            try {
+                Thread.sleep(500L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            final String sentence = Joiner.on(' ').join(args);
+            printSentenceParseTree(sentence);
         }
+    }
 
-        final String sentence = Joiner.on(' ').join(args);
+    private static void printSentenceParseTree(String sentence) {
         System.out.println("Sentence being analyzed: \"" + sentence + "\"\n---------------------------");
+
+        sentence = sentence.replaceAll("\\.", "");
 
         final Tree tree = parser.parse(sentence);
         final List<Tree> trees = tree.getChild(0).getChildrenAsList();
@@ -30,13 +46,15 @@ public class TaggingTest {
             System.out.print(part.label());
             System.out.println(part);
         }
+        tree.indentedListPrint();
 
         System.out.println();
 
         final List<Tree> leaves = tree.getLeaves();
-        for(final Tree leaf : leaves) {
+        for (final Tree leaf : leaves) {
             System.out.printf("(%s - %s), ", leaf.parent(tree).label(), leaf);
         }
+        System.out.println("\n");
 
         final List<String> lemmas = Lemmatizer.lemmatizeSentence(sentence);
         System.out.println(Joiner.on(' ').join(lemmas));
