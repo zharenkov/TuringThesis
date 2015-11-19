5f6775ba22d3662e9d4b3e83d071b23c0a3fe61d
Tyler Yates <tyler.yates@utexas.edu>
2015-11-18 19:19:50 -0600

    Calculate tense

--- src/main/java/generation/QuestionGenerator.java
+++ src/main/java/generation/QuestionGenerator.java
@@ -2,12 +2,14 @@ package generation;
 
 import simplenlg.features.Feature;
 import simplenlg.features.InterrogativeType;
+import simplenlg.features.Tense;
 import simplenlg.framework.NLGFactory;
 import simplenlg.lexicon.Lexicon;
 import simplenlg.phrasespec.NPPhraseSpec;
 import simplenlg.phrasespec.SPhraseSpec;
 import simplenlg.phrasespec.VPPhraseSpec;
 import simplenlg.realiser.english.Realiser;
+import tagging.StanfordParser;
 
 public class QuestionGenerator {
     private static final Lexicon lexicon = Lexicon.getDefaultLexicon();
@@ -15,10 +17,13 @@ public class QuestionGenerator {
     private static final Realiser realiser = new Realiser(lexicon);
 
     public static String generateLocationQuestion(String vp, String subject) {
+        final Tense tense = StanfordParser.calculateTense(vp);
         final VPPhraseSpec vpPhraseSpec = nlgFactory.createVerbPhrase(vp);
+        vpPhraseSpec.setFeature(Feature.TENSE, tense);
         final NPPhraseSpec npPhraseSpec = nlgFactory.createNounPhrase(subject);
 
         final SPhraseSpec sPhraseSpec = nlgFactory.createClause(npPhraseSpec, vpPhraseSpec);
+        sPhraseSpec.setFeature(Feature.TENSE, tense);
         sPhraseSpec.setFeature(Feature.INTERROGATIVE_TYPE, InterrogativeType.WHERE);
 
         return realiser.realiseSentence(sPhraseSpec);
--- src/main/java/tagging/StanfordParser.java
+++ src/main/java/tagging/StanfordParser.java
@@ -12,34 +12,49 @@ import edu.stanford.nlp.trees.PennTreebankLanguagePack;
 import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.trees.TreebankLanguagePack;
 import edu.stanford.nlp.trees.TypedDependency;
+import simplenlg.features.Tense;
 
 import java.io.StringReader;
 import java.util.Collection;
 import java.util.List;
 
 public class StanfordParser {
-    private final static String PCG_MODEL = "edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz";
+    private static final String PCG_MODEL = "edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz";
 
-    private final TokenizerFactory<CoreLabel> tokenizerFactory = PTBTokenizer.factory(new CoreLabelTokenFactory(),
-            "invertible=true");
-    private final LexicalizedParser parser = LexicalizedParser.loadModel(PCG_MODEL);
+    private static final TokenizerFactory<CoreLabel> tokenizerFactory = PTBTokenizer.factory(
+            new CoreLabelTokenFactory(), "invertible=true");
+    private static final LexicalizedParser parser = LexicalizedParser.loadModel(PCG_MODEL);
 
-    private Tree getPosTree(String sentence) {
+    private static Tree getPosTree(String sentence) {
         final Tokenizer<CoreLabel> tokenizer = tokenizerFactory.getTokenizer(new StringReader(sentence));
         final List<CoreLabel> tokens = tokenizer.tokenize();
         return parser.apply(tokens);
     }
 
-    private Collection<TypedDependency> getDependencies(Tree sentenceParseTree) {
+    private static Collection<TypedDependency> getDependencies(Tree sentenceParseTree) {
         final TreebankLanguagePack tlp = new PennTreebankLanguagePack();
         final GrammaticalStructureFactory gsf = tlp.grammaticalStructureFactory();
         final GrammaticalStructure gs = gsf.newGrammaticalStructure(sentenceParseTree);
         return gs.typedDependenciesCollapsed();
     }
 
-    public Sentence parseSentence(String sentence) {
+    public static Sentence parseSentence(String sentence) {
         final Tree posTree = getPosTree(sentence);
         return new Sentence(posTree, getDependencies(posTree),
                 StanfordNamedEntityRecognizer.findNamedEntities(sentence));
+    }
+
+    public static Tense calculateTense(String clause) {
+        final Tree posTree = getPosTree(clause);
+        for (Tree word : posTree.getLeaves()) {
+            final String pos = word.parent(posTree).label().value().toLowerCase();
+            if (pos.equals("md")) {
+                return Tense.FUTURE;
+            }
+            if (pos.equals("vbd") || pos.equals("vbn")) {
+                return Tense.PAST;
+            }
+        }
+        return Tense.PRESENT;
     }
 }
