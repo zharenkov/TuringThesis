8624b8412c3158112653d444a203e0b4f231d357
Tyler Yates <tyler.yates@utexas.edu>
2015-11-30 14:14:30 -0600

    Use Truth instead of Hamcrest.

--- src/test/java/question/QuestionAssertions.java
+++ src/test/java/question/QuestionAssertions.java
@@ -1,22 +1,18 @@
 package question;
 
-import org.hamcrest.collection.IsEmptyIterable;
-import org.hamcrest.collection.IsIterableContainingInAnyOrder;
-import tagging.StanfordParser;
+import static com.google.common.truth.Truth.assertThat;
+import static tagging.StanfordParser.parseSentence;
 
-import static org.junit.Assert.assertThat;
 
 /**
  * Static helper methods for making assertions about questions generated from rules.
  */
 class QuestionAssertions {
     static void assertQuestionCreated(Rule rule, String sentence, String... question) {
-        assertThat(rule.generateQuestions(StanfordParser.parseSentence(sentence)),
-                IsIterableContainingInAnyOrder.containsInAnyOrder(question));
+        assertThat(rule.generateQuestions(parseSentence(sentence))).containsExactly(question);
     }
 
     static void assertNoQuestionsCreated(Rule rule, String sentence) {
-        assertThat(rule.generateQuestions(StanfordParser.parseSentence(sentence)),
-                IsEmptyIterable.emptyIterable());
+        assertThat(rule.generateQuestions(parseSentence(sentence))).isEmpty();
     }
 }
