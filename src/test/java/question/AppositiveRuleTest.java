feaf88ceea6bd910802a2dbc640dd75dff2f44df
Tyler Yates <tyler.yates@utexas.edu>
2015-12-22 23:12:02 -0600

    Add test case with appositive at beginning of sentence

--- src/test/java/question/AppositiveRuleTest.java
+++ src/test/java/question/AppositiveRuleTest.java
@@ -19,4 +19,12 @@ public class AppositiveRuleTest {
                 "Barack Obama, the 44th president of the United States, was born in 1961.",
                 "What is Barack Obama?", "Who is the 44th president of the United States?");
     }
+
+    @Test
+    public void testGenerateQuestionsTennis() {
+        // TODO think of a way to make the 'A' not capitalized when generating the question
+        assertQuestionCreatedWithPunctuation(appositiveRule,
+                "A hot-tempered tennis player, Robbie began to charge the umpire after the bad call.",
+                "What is Robbie?", "Who is A hot-tempered tennis player?");
+    }
 }
