477b698a2d776fc1cf8b05d6e33c4a6895e51fdf
Tyler Yates <tyler.yates@utexas.edu>
2016-02-01 16:45:57 -0600

    Add example for getting mentions in Simple CoreNLP

--- src/main/java/tagging/SimpleCoreNLPTest.java
+++ src/main/java/tagging/SimpleCoreNLPTest.java
@@ -7,6 +7,7 @@ public class SimpleCoreNLPTest {
         final Sentence sentence = new Sentence(
                 "George Washington (February 22, 1732 – December 14, 1799) was the first President of the United States (1789–97).");
         System.out.println(sentence.nerTags());
+        System.out.println(sentence.mentions("PERSON"));
         System.out.println(sentence.dependencyGraph());
         System.out.println(sentence.lemmas());
         System.out.println(sentence.posTags());
