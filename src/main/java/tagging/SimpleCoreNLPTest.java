b2926a5642beddb600f0748974be539394502729
Tyler Yates <tyler.yates@utexas.edu>
2016-02-01 13:35:35 -0600

    Test for Simple CoreNLP

--- src/main/java/tagging/SimpleCoreNLPTest.java
+++ src/main/java/tagging/SimpleCoreNLPTest.java
@@ -1,7 +1,14 @@
 package tagging;
 
+import edu.stanford.nlp.simple.Sentence;
+
 public class SimpleCoreNLPTest {
     public static void main(String[] args) {
-
+        final Sentence sentence = new Sentence(
+                "George Washington (February 22, 1732 – December 14, 1799) was the first President of the United States (1789–97).");
+        System.out.println(sentence.nerTags());
+        System.out.println(sentence.dependencyGraph());
+        System.out.println(sentence.lemmas());
+        System.out.println(sentence.posTags());
     }
 }
