80a8e76c897964cceff994f87efb9e9e70da6120
Tyler Yates <tyler.yates@utexas.edu>
2016-02-06 19:23:40 -0600

    Add VBN as a key to detecting past tense

--- src/main/java/simplification/TenseUtil.java
+++ src/main/java/simplification/TenseUtil.java
@@ -5,7 +5,8 @@ import edu.stanford.nlp.simple.Sentence;
 public class TenseUtil {
     public static Tense calculateTense(Sentence sentence) {
         for (final String tag : sentence.posTags()) {
-            if (tag.equalsIgnoreCase("vbd")) {
+            // Look for past-tense verb or past participle
+            if (tag.equalsIgnoreCase("vbd") || tag.equalsIgnoreCase("vbn")) {
                 return Tense.PAST;
             }
         }
