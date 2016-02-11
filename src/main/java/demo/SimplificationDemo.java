ac99cce6a59cd295e39cc46fed957127313615d8
Tyler Yates <tyler.yates@utexas.edu>
2016-02-11 12:52:03 -0600

    Create serializable class to hold the results of simplification on the topic sentences.

--- src/main/java/demo/SimplificationDemo.java
+++ src/main/java/demo/SimplificationDemo.java
@@ -7,13 +7,10 @@ import java.io.FileNotFoundException;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class SimplificationDemo {
     private static final String TOPIC_SENTENCES_FILE_NAME = "topic_sentences.txt";
-    private static final String DIVIDER = "\n---------------------------------\n";
     private static final PrintStream OUT = System.out;
     private static final PrintStream ERR = System.err;
     private static final PrintStream DUMMY_STREAM = new PrintStream(new OutputStream() {
@@ -34,29 +31,18 @@ public class SimplificationDemo {
             sentences.add(scanner.nextLine());
         }
 
-        System.out.printf("Found %d sentences\n", sentences.size());
-
         System.setOut(DUMMY_STREAM);
         System.setErr(DUMMY_STREAM);
-        final List<List<String>> simplifiedSentences = new ArrayList<>();
-        int totalSimplifiedSentences = 0;
+        final Map<String, List<String>> sentenceToSimplifiedSentences = new LinkedHashMap<>();
         for (final String sentence : sentences) {
             final List<String> strings = SentenceSimplifier.simplifySentence(sentence);
-            simplifiedSentences.add(strings);
-            totalSimplifiedSentences += strings.size();
+            sentenceToSimplifiedSentences.put(sentence, strings);
         }
         System.setOut(OUT);
         System.setErr(ERR);
-        System.out.printf("%d total simplified sentences\n", totalSimplifiedSentences);
 
-        for (int i = 0; i < simplifiedSentences.size(); i++) {
-            final List<String> listOfSimplifiedSentences = simplifiedSentences.get(i);
-            System.out.println(DIVIDER);
-            System.out.println("Original Sentence: " + sentences.get(i) + "\n");
-            for (final String simplifiedSentece : listOfSimplifiedSentences) {
-                System.out.println(simplifiedSentece);
-            }
-            System.out.println(DIVIDER);
-        }
+        final TopicSentencesSimplification simplification = new TopicSentencesSimplification(
+                sentenceToSimplifiedSentences);
+        System.out.println(simplification);
     }
 }
---
+++ src/main/java/demo/TopicSentencesSimplification.java
@@ -0,0 +1,55 @@
+package demo;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class TopicSentencesSimplification implements Serializable {
+    private final Map<String, List<String>> sentenceToSimplifiedSentences;
+    private final int numberOfSimplifiedSentences;
+
+    public TopicSentencesSimplification(Map<String, List<String>> sentenceToSimplifiedSentences) {
+        this.sentenceToSimplifiedSentences = sentenceToSimplifiedSentences;
+        int numberOfSimplifiedSentences = 0;
+        for (final List<String> value : sentenceToSimplifiedSentences.values()) {
+            numberOfSimplifiedSentences += value.size();
+        }
+        this.numberOfSimplifiedSentences = numberOfSimplifiedSentences;
+    }
+
+    @Override
+    public int hashCode() {
+        return numberOfSimplifiedSentences;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        final TopicSentencesSimplification that = (TopicSentencesSimplification) o;
+
+        return sentenceToSimplifiedSentences.equals(that.sentenceToSimplifiedSentences);
+
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(String.format("%d topic sentences\n", sentenceToSimplifiedSentences.size()));
+        builder.append(String.format("%d simplified sentences\n", numberOfSimplifiedSentences));
+        for (Entry<String, List<String>> entry : sentenceToSimplifiedSentences.entrySet()) {
+            builder.append("---------------------------------\n\n");
+            builder.append("Original Sentence:\n");
+            builder.append(entry.getKey()).append("\n\n");
+            builder.append("Simplified Sentences:\n");
+            for (final String simplifiedSentence : entry.getValue()) {
+                builder.append(simplifiedSentence).append("\n");
+            }
+            builder.append("\n---------------------------------\n\n");
+        }
+
+        return builder.toString();
+    }
+}
