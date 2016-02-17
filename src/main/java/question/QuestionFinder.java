f6bfa050f220b8a10d5b3b2cfba5bb822fd54d8e
Tyler Yates <tyler.yates@utexas.edu>
2016-02-16 18:44:35 -0600

    Start from scratch on question generation to take advantage of Simple CoreNLP and the new realizer system wrapping SimpleNLG.

--- src/main/java/question/QuestionFinder.java
+++ src/main/java/question/QuestionFinder.java
@@ -1,23 +1,17 @@
 package question;
 
 import com.google.common.base.Joiner;
-import question.where.LocationRule;
-import question.who_what.CopulaRule;
-import question.who_what.NpVpRule;
-import tagging.ParsedSentence;
-import tagging.StanfordCoreNlpClient;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class QuestionFinder {
-    final static List<Rule> questionRules = Arrays.asList(new LocationRule(), new CopulaRule(), new NpVpRule());
+    final static List<Rule> questionRules = Arrays.asList();
 
     public static void main(String[] args) {
         final String sentence = Joiner.on(' ').join(args);
-        final ParsedSentence parsedSentence = StanfordCoreNlpClient.parseSentence(sentence, true);
         for (final Rule rule : questionRules) {
-            rule.generateQuestions(parsedSentence);
+            rule.generateQuestions(sentence);
         }
     }
 }
--- src/main/java/question/Rule.java
+++ src/main/java/question/Rule.java
@@ -1,23 +1,23 @@
 package question;
 
-import edu.stanford.nlp.trees.Tree;
-import tagging.ParsedSentence;
+import edu.stanford.nlp.simple.Sentence;
 
 import java.util.HashSet;
 import java.util.Set;
 
 public abstract class Rule {
-    public Set<String> generateQuestions(ParsedSentence sentence) {
+    public Set<String> generateQuestions(String sentence) {
         final Set<String> questions = new HashSet<>();
         System.out.println("Starting " + getRuleName() + " scanning\n-----------------------------------");
-        System.out.println("ParsedSentence: '" + sentence.getString() + "'");
-        System.out.println("NERs: " + sentence.getNamedEntities());
-        findQuestions(sentence.getPosTree(), sentence, questions);
+        System.out.println("Examining: '" + sentence + "'");
+        final Sentence parsedSentence = new Sentence(sentence);
+        findQuestions(parsedSentence, questions);
+        System.out.println("\nGenerated Questions:\n" + questions);
         System.out.println("-----------------------------------\nEnding " + getRuleName() + " scanning");
         return questions;
     }
 
-    protected abstract void findQuestions(Tree tree, ParsedSentence sentence, Set<String> questions);
+    protected abstract void findQuestions(Sentence sentence, Set<String> questions);
 
     protected abstract String getRuleName();
 }
