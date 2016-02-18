278aed195acc2b65a623e1bc485fdc655a6648e8
Tyler Yates <tyler.yates@utexas.edu>
2016-02-18 17:33:13 -0600

    Create Rules class to simplify invoking rule generation on all rules.

--- src/main/java/demo/QuestionGenerationDemo.java
+++ src/main/java/demo/QuestionGenerationDemo.java
@@ -1,20 +1,11 @@
 package demo;
 
 import com.google.common.base.Joiner;
-import question.CopulaRule;
-import question.Rule;
-
-import java.util.Arrays;
-import java.util.List;
+import question.Rules;
 
 public class QuestionGenerationDemo {
-    // Add all rules that should be used to generate questions here
-    final static List<Rule> questionRules = Arrays.asList(CopulaRule.getRule());
-
     public static void main(String[] args) {
         final String sentence = Joiner.on(' ').join(args);
-        for (final Rule rule : questionRules) {
-            rule.generateQuestions(sentence);
-        }
+        Rules.generateQuestions(sentence);
     }
 }
---
+++ src/main/java/question/Rules.java
@@ -0,0 +1,20 @@
+package question;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Rules {
+    // Add all rules that should be used to generate questions here
+    final static List<Rule> questionRules = Arrays.asList(CopulaRule.getRule());
+
+    public static Set<String> generateQuestions(String simplifiedSentence) {
+        final Set<String> generatedQuestions = new HashSet<>();
+        for (final Rule rule : questionRules) {
+            final Set<String> questions = rule.generateQuestions(simplifiedSentence);
+            generatedQuestions.addAll(questions);
+        }
+        return generatedQuestions;
+    }
+}
