430a5063ea0f086cf54c23c778e20aa7d3705f84
Tyler Yates <tyler.yates@utexas.edu>
2016-02-18 21:33:33 -0600

    Create demo for the full project pipeline: sentence simplification and question generation

---
+++ src/main/java/demo/FullPipelineDemo.java
@@ -0,0 +1,63 @@
+package demo;
+
+import simplification.SentenceSimplifier;
+
+import java.io.*;
+import java.net.URL;
+import java.util.*;
+
+public class FullPipelineDemo {
+    private static final String TOPIC_SENTENCES_FILE_NAME = "topic_sentences.txt";
+    private static final String OUTPUT_FILE_NAME = "output/demo/pipeline/result_%d.txt";
+    private static final PrintStream OUT = System.out;
+    private static final PrintStream ERR = System.err;
+    private static final PrintStream DUMMY_STREAM = new PrintStream(new OutputStream() {
+        public void write(int b) {
+        }
+    });
+
+    public static void main(String[] args) throws FileNotFoundException {
+        final ClassLoader classLoader = SimplificationDemo.class.getClassLoader();
+        final URL resource = classLoader.getResource(TOPIC_SENTENCES_FILE_NAME);
+        if (resource == null) {
+            System.err.println("Cannot load topic sentences");
+            return;
+        }
+        final Scanner scanner = new Scanner(new File(resource.getFile()));
+        final List<String> sentences = new ArrayList<>();
+        while (scanner.hasNext()) {
+            sentences.add(scanner.nextLine());
+        }
+
+        System.setOut(DUMMY_STREAM);
+        //System.setErr(DUMMY_STREAM);
+        final Map<String, List<String>> sentenceToSimplifiedSentences = new LinkedHashMap<>();
+        for (final String sentence : sentences) {
+            final List<String> strings = SentenceSimplifier.simplifySentence(sentence);
+            sentenceToSimplifiedSentences.put(sentence, strings);
+        }
+        final TopicSentencesSimplificationAndQuestions simplification = new TopicSentencesSimplificationAndQuestions(
+                sentenceToSimplifiedSentences);
+        System.setOut(OUT);
+        //System.setErr(ERR);
+        System.out.println(simplification);
+
+        if (args.length == 1 && args[0].equals("out")) {
+            int n = 0;
+            File file = new File(String.format(OUTPUT_FILE_NAME, n));
+            while (file.exists()) {
+                n++;
+                file = new File(String.format(OUTPUT_FILE_NAME, n));
+            }
+            try {
+                file.getParentFile().mkdirs();
+                file.createNewFile();
+                try (PrintWriter out = new PrintWriter(file)) {
+                    out.println(simplification.toString());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/main/java/demo/TopicSentencesSimplificationAndQuestions.java
@@ -0,0 +1,75 @@
+package demo;
+
+import question.Rules;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class TopicSentencesSimplificationAndQuestions implements Serializable {
+    private final Map<String, List<String>> sentenceToSimplifiedSentences;
+    private final Map<String, Set<String>> simplifiedSentenceToQuestions;
+    private final int numberOfSimplifiedSentences;
+    private final int numberOfGeneratedQuestions;
+
+    public TopicSentencesSimplificationAndQuestions(Map<String, List<String>> sentenceToSimplifiedSentences) {
+        this.sentenceToSimplifiedSentences = sentenceToSimplifiedSentences;
+        simplifiedSentenceToQuestions = new HashMap<>();
+        int numberOfSimplifiedSentences = 0;
+        int numberOfGeneratedQuestions = 0;
+        for (final List<String> value : sentenceToSimplifiedSentences.values()) {
+            numberOfSimplifiedSentences += value.size();
+            for (final String simplifiedSentence : value) {
+                final Set<String> generatedQuestions = Rules.generateQuestions(simplifiedSentence);
+                simplifiedSentenceToQuestions.put(simplifiedSentence, generatedQuestions);
+                numberOfGeneratedQuestions += generatedQuestions.size();
+            }
+        }
+        this.numberOfSimplifiedSentences = numberOfSimplifiedSentences;
+        this.numberOfGeneratedQuestions = numberOfGeneratedQuestions;
+    }
+
+    @Override
+    public int hashCode() {
+        return numberOfSimplifiedSentences;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        final TopicSentencesSimplificationAndQuestions that = (TopicSentencesSimplificationAndQuestions) o;
+
+        return sentenceToSimplifiedSentences.equals(that.sentenceToSimplifiedSentences);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(String.format("%d topic sentences\n", sentenceToSimplifiedSentences.size()));
+        builder.append(String.format("%d simplified sentences\n", numberOfSimplifiedSentences));
+        builder.append(String.format("%d generated questions\n", numberOfGeneratedQuestions));
+        for (Entry<String, List<String>> entry : sentenceToSimplifiedSentences.entrySet()) {
+            builder.append("---------------------------------\n\n");
+            builder.append("Original Sentence:\n");
+            builder.append(entry.getKey()).append("\n\n");
+            builder.append("Simplified Sentences:\n");
+            for (final String simplifiedSentence : entry.getValue()) {
+                builder.append(simplifiedSentence).append("\n");
+                final Set<String> questions = simplifiedSentenceToQuestions.get(simplifiedSentence);
+                if (questions != null) {
+                    for (final String question : questions) {
+                        builder.append("\t").append(question).append("\n");
+                    }
+                }
+            }
+            builder.append("\n---------------------------------\n\n");
+        }
+
+        return builder.toString();
+    }
+}
