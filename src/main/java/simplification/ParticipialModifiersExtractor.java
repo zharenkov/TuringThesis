643a3ecd8c5a7def93334b8af3ebd25d179b4c99
Tyler Yates <tyler.yates@utexas.edu>
2016-02-25 15:27:41 -0600

    Experimental amplification of the ParticipialModifierExtractor.

--- src/main/java/simplification/ParticipialModifiersExtractor.java
+++ src/main/java/simplification/ParticipialModifiersExtractor.java
@@ -1,15 +1,28 @@
 package simplification;
 
 import com.google.common.base.Joiner;
-import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Range;
 import com.google.common.collect.RangeSet;
 import com.google.common.collect.TreeRangeSet;
+import edu.stanford.nlp.ling.IndexedWord;
+import edu.stanford.nlp.semgraph.SemanticGraph;
+import edu.stanford.nlp.semgraph.SemanticGraphEdge;
 import edu.stanford.nlp.simple.Sentence;
+import edu.stanford.nlp.trees.Tree;
 import generation.TextRealization;
+import simplenlg.features.Tense;
+import util.TenseUtil;
+import util.TreeUtil;
 import util.WordListUtil;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import static edu.stanford.nlp.trees.UniversalEnglishGrammaticalRelations.ADV_CLAUSE_MODIFIER;
+import static edu.stanford.nlp.trees.UniversalEnglishGrammaticalRelations.NOMINAL_SUBJECT;
+import static generation.TextRealization.realizeSentence;
+import static generation.TextRealization.realizeVerbPhraseWithFeatures;
 
 public class ParticipialModifiersExtractor implements Extractor {
     private static ParticipialModifiersExtractor extractor;
@@ -25,41 +38,72 @@ public class ParticipialModifiersExtractor implements Extractor {
     }
 
     public static void main(String[] args) {
-        getExtractor().extract(Joiner.on(' ').join(args));
+        final SimplificationResult simplificationResult = getExtractor().extract(Joiner.on(' ').join(args));
+        System.out.println(simplificationResult.getSimplifiedSentences());
     }
 
     @Override
     public SimplificationResult extract(String sentence) {
+        System.out.println("Original sentence: " + sentence);
         final Sentence parsed = new Sentence(sentence);
+        final Tree root = parsed.parse();
+        final SemanticGraph dependencyGraph = parsed.dependencyGraph();
         final List<String> words = parsed.words();
-        final List<String> nerTags = parsed.nerTags();
         final List<String> posTags = parsed.posTags();
 
+        final List<SemanticGraphEdge> relations = dependencyGraph.findAllRelns(ADV_CLAUSE_MODIFIER);
+        System.out.println("Relations: " + relations);
+
+        final Set<String> simplifiedSentences = new HashSet<>();
         final RangeSet<Integer> rangeSet = TreeRangeSet.create();
-        for (int i = 1; i < posTags.size(); i++) {
+        for (int i = 0; i < posTags.size(); i++) {
             final String posTag = posTags.get(i);
             if (posTag.equalsIgnoreCase("vbg") || posTag.equalsIgnoreCase("vbn")) {
-                if (words.get(i - 1).equalsIgnoreCase(",")) {
-                    int rightCommaBound = words.size() - 1;
-                    for (int k = i + 1; k < words.size(); k++) {
-                        if (words.get(k).equalsIgnoreCase(",") && !nerTags.get(k).equalsIgnoreCase("date")) {
-                            rightCommaBound = k;
-                            break;
+                // Participial phrases should not come after a verb
+                if (i > 0 && !posTags.get(i - 1).toLowerCase().startsWith("vb")) {
+                    final Tree word = root.getLeaves().get(i);
+                    final Tree participialPhrase = TreeUtil.getVpFromWord(root, word);
+
+                    // Calculate the bounds of the participial phrase and account for commas
+                    final Range<Integer> rangeOfParticipialPhrase = TreeUtil.getRangeOfTree(parsed, participialPhrase);
+                    int lowerBound = rangeOfParticipialPhrase.lowerEndpoint();
+                    if (i > 0 && words.get(i - 1).equals(",")) {
+                        lowerBound--;
+                    }
+                    int upperBound = rangeOfParticipialPhrase.upperEndpoint();
+                    if (i < posTags.size() - 1 && words.get(i + 1).equals(",")) {
+                        upperBound++;
+                    }
+                    rangeSet.add(Range.closed(lowerBound, upperBound));
+
+                    for (final SemanticGraphEdge edge : relations) {
+                        final IndexedWord dependent = edge.getDependent();
+                        final int dependentIndex = dependent.index() - 1;
+                        if (rangeOfParticipialPhrase.contains(dependentIndex)) {
+                            final List<SemanticGraphEdge> governorRelations = dependencyGraph.getOutEdgesSorted(
+                                    edge.getGovernor());
+                            for (final SemanticGraphEdge governorEdge : governorRelations) {
+                                if (governorEdge.getRelation().equals(NOMINAL_SUBJECT)) {
+                                    final IndexedWord subject = governorEdge.getDependent();
+                                    final Tree subjectNp = TreeUtil.getNpFromWord(root, subject);
+                                    final String subjectNpString = TreeUtil.constructPhraseFromTree(subjectNp);
+
+                                    final String vp = TreeUtil.constructPhraseFromTree(participialPhrase);
+                                    final Tense tense = TenseUtil.calculateTense(parsed);
+                                    final String participialPhraseModified = realizeVerbPhraseWithFeatures(vp, false,
+                                            tense);
+                                    final String simplifiedSentence = realizeSentence(subjectNpString,
+                                            participialPhraseModified);
+                                    simplifiedSentences.add(simplifiedSentence);
+                                }
+                            }
                         }
                     }
-                    while (rightCommaBound < words.size() && !words.get(rightCommaBound).equalsIgnoreCase(",")) {
-                        rightCommaBound++;
-                    }
-                    rangeSet.add(Range.closed(i - 1, rightCommaBound));
-
                 }
             }
         }
-        if (rangeSet.isEmpty()) {
-            return new SimplificationResult(ImmutableSet.of(sentence));
-        }
-        final String simplifiedSentence = TextRealization.realizeSentence(WordListUtil.constructPhraseFromWordList(
-                WordListUtil.removeParts(words, rangeSet)));
-        return new SimplificationResult(ImmutableSet.of(simplifiedSentence));
+        final String phrase = WordListUtil.constructPhraseFromWordList(WordListUtil.removeParts(words, rangeSet));
+        simplifiedSentences.add(TextRealization.realizeSentence(phrase));
+        return new SimplificationResult(simplifiedSentences);
     }
 }
