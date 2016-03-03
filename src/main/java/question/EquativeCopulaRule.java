cca66a4c4c76ed74e06cf01181ec037a6a5b830c
Tyler Yates <tyler.yates@utexas.edu>
2016-03-03 00:13:05 -0600

    Change CopulaRule to EquativeCopulaRule.

--- src/main/java/question/EquativeCopulaRule.java
+++ src/main/java/question/EquativeCopulaRule.java
@@ -1,21 +1,18 @@
 package question;
 
-import com.google.common.collect.ImmutableSet;
+import com.google.common.base.Joiner;
 import edu.stanford.nlp.simple.Sentence;
 import edu.stanford.nlp.trees.Tree;
 import generation.TextRealization;
-import util.NerUtil;
-import util.ReversePhraseBuilder;
 import util.TreeUtil;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import static util.TreeUtil.*;
+import static util.NerUtil.headOfTreeIsPerson;
 
 public class EquativeCopulaRule extends Rule {
-    private static final Set<String> COPULAS = ImmutableSet.of("be");
-
     private static EquativeCopulaRule instance;
 
     private EquativeCopulaRule() {
@@ -28,72 +25,55 @@ public class EquativeCopulaRule extends Rule {
         return instance;
     }
 
-    @Override
-    protected void findQuestions(Sentence sentence, Set<String> questions) {
-        final List<String> lemmas = sentence.lemmas();
-        System.out.println(lemmas);
-        for (int i = 0; i < lemmas.size(); i++) {
-            final String lemma = lemmas.get(i);
-            if (COPULAS.contains(lemma)) {
-                System.out.println("Copula detected at index " + i);
-                final Tree root = sentence.parse();
-                final Tree verbTree = root.getLeaves().get(i);
-                final Tree verbPhraseTree = getParent(root, verbTree, 2);
-
-                if (containsOnlyVerbsAndAdverbs(verbPhraseTree)) {
-                    break;
-                }
-
-                final Tree rightHead = TreeUtil.findHead(verbPhraseTree);
-                final String rightNp = TreeUtil.getStringAfterTree(root, verbTree);
-                final ReversePhraseBuilder verbPhrase = new ReversePhraseBuilder();
-
-                Tree upperMostVerbPhrase = verbPhraseTree;
-                Tree phraseAboveUpperMostVerbPhrase = verbPhraseTree;
-                while (labelEquals(phraseAboveUpperMostVerbPhrase, "vp")) {
-                    // Only add the first child if it is not a verb phrase
-                    if (!labelEquals(phraseAboveUpperMostVerbPhrase.firstChild(), "vp")) {
-                        verbPhrase.addString(phraseAboveUpperMostVerbPhrase.getLeaves().get(0).label().value());
-                    }
-                    upperMostVerbPhrase = phraseAboveUpperMostVerbPhrase;
-                    phraseAboveUpperMostVerbPhrase = getParent(root, phraseAboveUpperMostVerbPhrase);
-                }
-
-                final List<Tree> children = phraseAboveUpperMostVerbPhrase.getChildrenAsList();
-                final int indexOfUpperMostVerbPhrase = phraseAboveUpperMostVerbPhrase.objectIndexOf(
-                        upperMostVerbPhrase);
-                if (indexOfUpperMostVerbPhrase > 0) {
-                    final Tree leftOfVerbPhrase = children.get(indexOfUpperMostVerbPhrase - 1);
-                    if (leftOfVerbPhrase.label().value().equalsIgnoreCase("np")) {
-                        final Tree leftHead = TreeUtil.findHead(leftOfVerbPhrase);
-                        final String leftNp = TreeUtil.constructPhraseFromTree(leftOfVerbPhrase);
-                        final int leftIndex = TreeUtil.getLeafIndex(root, leftHead.getLeaves().get(0));
-                        final int rightIndex = TreeUtil.getLeafIndex(root, rightHead.getLeaves().get(0));
-                        final String wh;
-                        if (NerUtil.isPerson(sentence, leftIndex) || NerUtil.isPerson(sentence, rightIndex)) {
-                            wh = "Who";
-                        } else {
-                            wh = "What";
-                        }
-                        questions.add(TextRealization.realizeQuestion(wh, verbPhrase.toString(), rightNp));
-                        questions.add(TextRealization.realizeQuestion(wh, verbPhrase.toString(), leftNp));
-                    }
-                }
-            }
-        }
+    public static void main(String[] args) {
+        final HashSet<String> questions = new HashSet<>();
+        getRule().findQuestions(new Sentence(Joiner.on(' ').join(args)), questions);
+        System.out.println(questions);
     }
 
-    private static boolean containsOnlyVerbsAndAdverbs(Tree tree) {
-        for (final Tree child : tree.children()) {
-            if (!labelContains(child, "vp") && !labelStartsWith(child, "vb")) {
-                return false;
+    @Override
+    protected void findQuestions(Sentence sentence, Set<String> questions) {
+        final Tree root = sentence.parse();
+        final List<String> words = sentence.words();
+        final List<String> lemmas = sentence.lemmas();
+        for (int i = 1; i < lemmas.size(); i++) {
+            final String lemma = lemmas.get(i);
+            if (lemma.equals("be")) {
+                final Tree governorTree = TreeUtil.getHighestNpFromWord(root, i - 1);
+                System.out.println(governorTree);
+                final Tree beVpTree = TreeUtil.getVpFromWord(root, root.getLeaves().get(i));
+                System.out.println(beVpTree);
+                if (governorTree != null && beVpTree != null && beVpTree.numChildren() > 1) {
+                    final Tree dependentTree = beVpTree.getChild(1);
+                    if (TreeUtil.labelEquals(dependentTree, "np")) {
+                        final String governorString = TreeUtil.constructPhraseFromTree(governorTree);
+                        final StringBuilder dependentString = new StringBuilder(
+                                TreeUtil.constructPhraseFromTree(dependentTree));
+                        for (int k = 2; k < beVpTree.numChildren(); k++) {
+                            final Tree child = beVpTree.getChild(k);
+                            if (TreeUtil.labelEquals(child, "vp")) {
+                                break;
+                            }
+                            dependentString.append(" ").append(TreeUtil.constructPhraseFromTree(child));
+                        }
+
+                        final String wh;
+                        if (headOfTreeIsPerson(sentence, root, governorTree) || headOfTreeIsPerson(sentence, root,
+                                dependentTree)) {
+                            wh = "who";
+                        } else {
+                            wh = "what";
+                        }
+                        questions.add(TextRealization.realizeQuestion(wh, words.get(i), governorString));
+                        questions.add(TextRealization.realizeQuestion(wh, words.get(i), dependentString.toString()));
+                    }
+                }
             }
         }
-        return true;
     }
 
     @Override
     protected String getRuleName() {
-        return "Copula Rule";
+        return "Equative Copula Rule";
     }
 }
